187b63c7c504b738da14ddf5e022f886efcd050e
pazzispb20 <pazzispb20@gmail.com>
2024-09-02 22:22:37 -0400

    validaciones para los esquemas SignUp y SignIn

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,26 +1,46 @@
 import { z } from "zod";
 export const SignUpSchema = z
   .object({
-    nombre: z.string().min(2).max(50),
-    apellido: z.string().min(2).max(50),
-    direccion: z.string().min(10).max(100),
-    correo_electronico: z.string().email().min(10).max(100),
+    nombre: z
+      .string()
+      .min(2, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(50, { message: "El máximo de caracteres es 50" }),
+    apellido: z
+      .string()
+      .min(2, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(50, { message: "El máximo de caracteres es 50" }),
+    direccion: z
+      .string()
+      .min(10, { message: "Debe tener como mínimo 10 caracteres" })
+      .max(100, { message: "El máximo de caracteres es 100" }),
+    correo_electronico: z
+      .string()
+      .email({ message: "Correo electrónico no válido" })
+      .min(10, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(100, { message: "El máximo de caracteres es 100" }),
     password: z
       .string()
-      .min(8, { message: "Password must be at least 8 characters long" }),
+      .min(8, { message: "La contraseña debe tener como mínimo 8 caracteres" })
+      .max(20, { message: "El máximo de caracteres es 20" }),
     confirmPassword: z
       .string()
-      .min(8, { message: "Password must be at least 8 characters long" }),
+      .min(8, { message: "La contraseña debe tener como mínimo 8 caracteres" })
+      .max(20, { message: "El máximo de caracteres es 20" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
+    message: "Las contraseñas con coinciden",
     path: ["confirmPassword"],
   });
 
 
 export const SignInSchema = z.object({
-  nombre: z.string().min(2).max(50),
+  correo_electronico: z
+    .string()
+    .email({ message: "Correo electrónico no válido" })
+    .min(10, { message: "Debe tener como mínimo 10 caracteres" })
+    .max(100, { message: "El máximo de caracteres es 100" }),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters long" }),
+    .min(8, { message: "La contraseña debe tener como mínimo 8 caracteres" })
+    .max(20, { message: "El máximo de caracteres es 20" }),
 });
