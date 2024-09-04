b9ef4a1e404f170322e4e62be1456fa75431d718
pazzispb20 <pazzispb20@gmail.com>
2024-09-03 21:55:54 -0400

    mejora en la distribucion de los elementos en pantalla

--- src/app/(socialmood)/sign-up/page.tsx
+++ src/app/(socialmood)/sign-up/page.tsx
@@ -12,15 +12,20 @@ export default async function SignUpPage() {
 
   return (
     <div>
-      <div className="absolute top-[5%] left-[45%] flex flex-col items-center justify-center space-y-10 rounded-lg bg-[#1c1b2a] p-6 z-10">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+                      flex flex-col items-center justify-center 
+                      p-10 z-10 w-[600px]
+                      bg-gradient-to-b from-white/30 backdrop-blur-sm drop-shadow-xl
+                      border border-white/80 rounded-[50px]"
+      >
         <SignUpForm />
       </div>
-      <Image className="absolute top-[40%] left-[32%] z-20" src={"/magic-wand.svg"} width={200} height={200} alt={""} />
-      <Image className="absolute bottom-10 left-[32%] z-0" src={"/fearful-face.svg"} height={200} width={200} alt={""} />
-      <Image className="absolute top-20 right-[28%] z-0" src={"/grinning-face.svg"} height={200} width={200} alt={""} />
-      <Image className="absolute top-10 left-[32%] z-0" src={"/pouting-face.svg"} height={200} width={200} alt={""} />
-      <Image className="absolute bottom-20 right-[32%] z-0" src={"/smiling-face-with-hearts.svg"} height={75} width={75} alt={""} />
-      <Image className="absolute top-[40%] right-[32%] z-0" src={"/sparkles.svg"} height={200} width={200} alt={""} />
+      <Image className="absolute top-[40%] left-[20%] z-20" src={"/magic-wand.svg"} width={200} height={200} alt={""} />
+      <Image className="absolute bottom-10 left-[20%] z-0" src={"/fearful-face.svg"} height={200} width={200} alt={""} />
+      <Image className="absolute top-20 right-[20%] z-0" src={"/grinning-face.svg"} height={200} width={200} alt={""} />
+      <Image className="absolute top-10 left-[22%] z-0" src={"/pouting-face.svg"} height={200} width={200} alt={""} />
+      <Image className="absolute bottom-20 right-[22%] z-0" src={"/smiling-face-with-hearts.svg"} height={75} width={75} alt={""} />
+      <Image className="absolute top-[40%] right-[20%] z-0" src={"/sparkles.svg"} height={200} width={200} alt={""} />
     </div>
   );
 }
