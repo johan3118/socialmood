3afefb4a0786d04f69cd6d54fc90543363d41591
Allen R. Silverio Olivo <allensilverioo@gmail.com>
2024-09-08 12:08:02 -0400

    cambio a ENV y fetch

--- src/app/services/paypalService.ts
+++ src/app/services/paypalService.ts
@@ -1,44 +1,62 @@
-import axios from 'axios';
-import {v4 as uuidv4} from 'uuid';
+"use server"
+import { v4 as uuidv4 } from 'uuid';
+import "dotenv/config";
+import dotenv from "dotenv";
 
-const clientId = 'AQEpcxi6zo0JHfCkgJafrgfVG1xeUNwk53-xUepwT2CpcV7_foTYlsxCjp-JngT_stubauCGq07u67Af';
-const clientSecret = 'ELQQQ82L4aOZ8JSalDsTrKNzCEiAOdBMVZXAPrT4N5x4Gt8IWYf1fOIiCoqsC2uYzH3-2MO-2yXlYTSM';
-const paypalBaseUrl = 'https://api-m.sandbox.paypal.com'; 
+dotenv.config({
+  path: ".env.local",
+});
 
+const clientId = process.env.PAYPAL_CLIENT_ID;
+const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+const paypalBaseUrl = process.env.PAYPAL_BASE_URL;
+
+// Verificar que las variables de entorno estén definidas
+if (!clientId || !clientSecret || !paypalBaseUrl) {
+  throw new Error('PayPal environment variables are not defined.');
+}
+
+// Función para obtener el token de acceso
 export const getAccessToken = async (): Promise<string> => {
-  const response = await axios.post(
-    `${paypalBaseUrl}/v1/oauth2/token`,
-    'grant_type=client_credentials',
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      auth: {
-        username: clientId,
-        password: clientSecret,
-      },
-    }
-  );
+  const authString = `${clientId}:${clientSecret}`;
+  const authBase64 = Buffer.from(authString).toString('base64');
 
-  console.log(response);
+  const response = await fetch(`${paypalBaseUrl}/v1/oauth2/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `Basic ${authBase64}`,
+    },
+    body: 'grant_type=client_credentials',
+  });
 
-  return response.data.access_token;
+  if (!response.ok) {
+    throw new Error(`Error fetching access token: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.access_token;
 };
 
+// Función para crear el plan de suscripción
 export const createSubscriptionPlan = async (accessToken: string, planData: any): Promise<any> => {
   const uuid = uuidv4();
-  const response = await axios.post(
-    `${paypalBaseUrl}/v1/billing/plans`,
-    planData,
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-        'PayPal-Request-Id': `${uuid}` // Puedes generar un ID único aquí
-      }
-    }
-  );
 
-  return response.data;
+  const response = await fetch(`${paypalBaseUrl}/v1/billing/plans`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+      'PayPal-Request-Id': uuid, // ID único
+    },
+    body: JSON.stringify(planData),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error creating subscription plan: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data;
 };
