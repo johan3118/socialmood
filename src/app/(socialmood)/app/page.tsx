980d383acd5623a3c6735621acd6720db2db3537
pazzispb <pazzispb20@gmail.com>
2024-09-10 16:36:15 -0400

    protected route added

--- src/app/(socialmood)/app/page.tsx
+++ src/app/(socialmood)/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   }
 
   return (
-    <div className="h-screen w-full flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-screen w-full flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
