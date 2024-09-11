547109148529396c4bce513b083148d0f8d8a6ee
pazzispb <pazzispb20@gmail.com>
2024-09-10 20:54:26 -0400

    listado de planes de subscripcion agregada

---
+++ src/app/(backoffice)/bo/(protected)/sub-table/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PlanesSubscripcionTable from '@/components/(backoffice)/subs-plan-table'
+
+function ListadoSubscripcionesPage() {
+  return (
+    <div>
+        <PlanesSubscripcionTable/>
+    </div>
+  )
+}
+
+export default ListadoSubscripcionesPage
--- src/app/actions/(backoffice)/subscriptions.actions.ts
+++ src/app/actions/(backoffice)/subscriptions.actions.ts
@@ -1,32 +1,55 @@
 "use server"
 import db from "@/db";
-import { planesTable } from "@/db/schema/socialMood";
+import {planesTable, tiposFacturacionTable, estadosPlanTable} from "@/db/schema/socialMood";
+import {eq} from "drizzle-orm";
 
 export const insertPlan = async (plan: {
-  nombre: string,
-  costo: number,
-  cantidad_interacciones_mes: number,
-  cantidad_usuarios_permitidos: number,
-  cantidad_cuentas_permitidas: number,
-  descripcion: string,
-  id_estado_plan: number,
-  id_tipo_facturacion: number,
-  paypal_plan_id: string
-}) => {
-  const insertedPlan = await db
+    nombre: string,
+    costo: number,
+    cantidad_interacciones_mes: number,
+    cantidad_usuarios_permitidos: number,
+    cantidad_cuentas_permitidas: number,
+    descripcion: string,
+    id_estado_plan: number,
+    id_tipo_facturacion: number,
+    paypal_plan_id: string
+  }) => {
+   const insertedPlan = await db
     .insert(planesTable)
     .values({
-      nombre: plan.nombre,
-      costo: plan.costo,
-      cantidad_interacciones_mes: plan.cantidad_interacciones_mes,
-      cantidad_usuarios_permitidos: plan.cantidad_usuarios_permitidos,
-      cantidad_cuentas_permitidas: plan.cantidad_cuentas_permitidas,
-      descripcion: plan.descripcion,
-      id_estado_plan: plan.id_estado_plan,
-      id_tipo_facturacion: plan.id_tipo_facturacion,
-      paypal_plan_id: plan.paypal_plan_id
-    })
+        nombre: plan.nombre,
+        costo: plan.costo,
+        cantidad_interacciones_mes: plan.cantidad_interacciones_mes,
+        cantidad_usuarios_permitidos: plan.cantidad_usuarios_permitidos,
+        cantidad_cuentas_permitidas: plan.cantidad_cuentas_permitidas,
+        descripcion: plan.descripcion,
+        id_estado_plan: plan.id_estado_plan,
+        id_tipo_facturacion: plan.id_tipo_facturacion,
+        paypal_plan_id: plan.paypal_plan_id
+      })
 
-  return JSON.parse(JSON.stringify(insertedPlan));
+      return JSON.parse(JSON.stringify(insertedPlan));
+    
+    };
 
-};
+export const selectAllPlans = async () => {
+  const allPlans = await db
+      .select({
+        planId: planesTable.id,
+        planNombre: planesTable.nombre,
+        costo: planesTable.costo,
+        cantidad_interacciones_mes: planesTable.cantidad_interacciones_mes,
+        cantidad_usuarios_permitidos: planesTable.cantidad_usuarios_permitidos,
+        cantidad_cuentas_permitidas: planesTable.cantidad_cuentas_permitidas,
+        estado_plan_nombre: estadosPlanTable.nombre,
+        tipo_facturacion_nombre: tiposFacturacionTable.nombre,
+        
+      })
+      .from(planesTable)
+      .innerJoin(estadosPlanTable, eq(planesTable.id_estado_plan, estadosPlanTable.id))
+      .innerJoin(tiposFacturacionTable, eq(planesTable.id_tipo_facturacion, tiposFacturacionTable.id))
+      .all();
+
+return JSON.parse(JSON.stringify(allPlans));
+  
+}
