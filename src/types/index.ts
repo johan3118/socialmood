d40dcbfdabfa33bbfa805ca4ccab6aab2ac7da29
pazzispb <pazzispb20@gmail.com>
2024-09-10 20:15:11 -0400

    tipo de crear usuario agregado

--- src/types/index.ts
+++ src/types/index.ts
@@ -73,3 +73,37 @@ export type SizeType =
 export type IconType = "ig" | "fb" | "gg" | "ag" | "angry";
 
 export type ButtonType = "button" | "submit" | "reset";
+
+export const CreateUserSchema = z
+  .object({
+    nombre: z
+      .string()
+      .min(2, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(50, { message: "El máximo de caracteres es 50" }),
+    apellido: z
+      .string()
+      .min(2, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(50, { message: "El máximo de caracteres es 50" }),
+    direccion: z
+      .string()
+      .min(10, { message: "Debe tener como mínimo 10 caracteres" })
+      .max(100, { message: "El máximo de caracteres es 100" }),
+    correo: z
+      .string()
+      .email({ message: "Correo electrónico no válido" })
+      .min(10, { message: "Debe tener como mínimo 2 caracteres" })
+      .max(100, { message: "El máximo de caracteres es 100" }),
+    password: z
+      .string()
+      .min(8, { message: "La contraseña debe tener como mínimo 8 caracteres" })
+      .max(20, { message: "El máximo de caracteres es 20" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "La contraseña debe tener como mínimo 8 caracteres" })
+      .max(20, { message: "El máximo de caracteres es 20" }),
+    tipoUsuario: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Las contraseñas con coinciden",
+    path: ["confirmPassword"],
+  });
