04c2b2aad2d66b0401a7dd46e0337dda0e218286
Paola <saldanaquezada@gmail.com>
2024-09-17 19:19:12 -0400

    cambios en tabla de interacciones

--- src/components/(socialmood)/listado-interacciones-table.tsx
+++ src/components/(socialmood)/listado-interacciones-table.tsx
@@ -26,9 +26,9 @@ const ListadoInteraccionesTable: React.FC = () => {
   const router = useRouter();
 
   const socialIconMap: { [key: string]: string } = {
-    Instagram: "/instagram.svg", 
-    Facebook: "/facebook.svg",   
-    Twitter: "/twitter.svg",    
+    Instagram: "/instagram.svg",
+    Facebook: "/facebook.svg",
+    Twitter: "/twitter.svg",
   };
 
   const emojimap: Record<string, string> = {
@@ -38,7 +38,7 @@ const ListadoInteraccionesTable: React.FC = () => {
     "Queja": "/angry.svg",
     "Elogio": "/happy.svg",
     "Recomendacion": "/happy.svg",
-    "Consulta": "/neutral-face.svg",   
+    "Consulta": "/neutral-face.svg",
     // categorías y subcategorias con sus respectivos emojis
   };
 
@@ -64,7 +64,7 @@ const ListadoInteraccionesTable: React.FC = () => {
   };
 
   return (
-    <div className="bg-gradient-to-b from-white/20 via-white/10 to-white/5 text-white border border-white/30 rounded-[32px] px-10 py-8">
+    <div className="bg-gradient-to-b from-white/20 via-white/10 to-white/5 text-white border border-white/30 rounded-[32px] px-10 mx-12 py-8">
       <div className="container mx-auto p-6">
         <div className="flex justify-between mb-6">
           <h1 className="text-[24px] text-white font-bold">Interacciones Capturadas</h1>
@@ -80,80 +80,76 @@ const ListadoInteraccionesTable: React.FC = () => {
           </button>
         </div>
         <hr className="border-[#FFF] mb-6" />
-        <table className="min-w-full table-fixed ">
-          <thead className="">
-            <tr className="text-[18px]">
-              <th className="py-6 px-10 text-left">Perfil</th>
-              <th className="py-6 px-10 text-left">Mensaje</th>
-              <th className="py-6 px-10 text-left">Emisor</th>
-              <th className="py-6 px-10 text-left">Categoria</th>
-              <th className="py-6 px-10 text-left">Subcategoria</th>
-              <th className="py-6 px-10 text-left">Fecha</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Interacciones.map((interacciones) => (
-              <tr>
-<td className="py-4 px-8">
-  <div className="flex items-center justify-center space-x-2 w-full">
-    <span
-      className={cn(buttonVariants({
-        variant:
-          interacciones.perfil.red_social === "Instagram"
-            ? "blue"
-            : interacciones.perfil.red_social === "Facebook"
-            ? "orange"
-            : "default",
-        size: "smBold"
-      }), "w-full flex justify-start items-center py-2")}
-    >
-      <img
-        src={socialIconMap[interacciones.perfil.red_social] || "/default.svg"} // Ícono correspondiente a la red social
-        alt={`${interacciones.perfil.red_social} Icon`}
-        className="flex justify-left mr-2"
-      />
-      {interacciones.perfil.username} 
-    </span>
-  </div>
-</td>
-                <td className="py-6 px-6 text-left">{interacciones.mensaje}</td>
-                <td className="py-6 px-6 text-left">{interacciones.emisor}</td>
-                <td className="py-3 px-4 font-bold text-left">
-                  <div className="flex items-center  justify-center space-x-2">
-                    <span
-                      className={cn(buttonVariants({ variant: "angry", size: "smBold" }))}
-                      style={{ width: "100%", justifyContent: "center" }} 
-
-                    >
-                      <img
-                        src={emojimap[interacciones.categoria] || "/default.svg"}  // Aquí usamos el mapeo
-                        alt="Emoji Icon"
-                        className="w-8 h-8 ml-2"
-                      />
-                      {interacciones.categoria}
-                    </span>
-                  </div>
-                </td>
-                <td className="py-3 px-8 font-bold text-left">
-                  <div className="flex items-center justify-center space-x-2">
-                    <span
-                      className={cn(buttonVariants({ variant: "angry", size: "smBold" }))}
-                      style={{ width: "100%", justifyContent: "center" }} 
-
-                    >
-                      <img
-                        src={emojimap[interacciones.subcategoria] || "/default.svg"} 
-                        alt="Emoji Icon"
-                        className="w-8 h-8 ml-2"
-                      />
-                      {interacciones.subcategoria}
-                    </span>
-                  </div>
-                </td>
-                <td className="py-6 px-6 text-left">{interacciones.fecha}</td>
-              </tr>
-            ))}
-          </tbody>
+        <table className="min-w-full table-auto ">
+          <thead>
+  <tr className="text-[16px] md:text-[18px]">
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left">Perfil</th>
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left">Mensaje</th>
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left hidden sm:table-cell">Emisor</th>
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left">Categoría</th>
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left">Subcategoría</th>
+    <th className="py-4 md:py-6 px-4 md:px-10 text-left hidden sm:table-cell">Fecha</th>
+  </tr>
+</thead>
+<tbody>
+  {Interacciones.map((interacciones) => (
+    <tr>
+      <td className="py-3 md:py-4 px-4 md:px-8">
+        <div className="flex items-center justify-center space-x-2 w-full">
+          <span
+            className={cn(
+              buttonVariants({
+                variant: interacciones.perfil.red_social === "Instagram" ? "blue" : interacciones.perfil.red_social === "Facebook" ? "orange" : "default",
+                size: "smBold",
+              }),
+              "w-full flex justify-start items-center py-2"
+            )}
+          >
+            <img
+              src={socialIconMap[interacciones.perfil.red_social] || "/default.svg"}
+              alt={`${interacciones.perfil.red_social} Icon`}
+              className="flex justify-left mr-2"
+            />
+            {interacciones.perfil.username}
+          </span>
+        </div>
+      </td>
+      <td className="py-4 md:py-6 px-4 md:px-6 text-left">{interacciones.mensaje}</td>
+      <td className="py-4 md:py-6 px-4 md:px-6 text-left hidden sm:table-cell">{interacciones.emisor}</td>
+      <td className="py-3 px-4 font-bold text-left">
+        <div className="flex items-center justify-center space-x-2">
+          <span
+            className={cn(buttonVariants({ variant: "angry", size: "smBold" }))}
+            style={{ width: "100%", justifyContent: "center" }}
+          >
+            <img
+              src={emojimap[interacciones.categoria] || "/default.svg"}
+              alt="Emoji Icon"
+              className="w-6 md:w-8 h-6 md:h-8 ml-2"
+            />
+            {interacciones.categoria}
+          </span>
+        </div>
+      </td>
+      <td className="py-3 px-4 md:px-8 font-bold text-left">
+        <div className="flex items-center justify-center space-x-2">
+          <span
+            className={cn(buttonVariants({ variant: "angry", size: "smBold" }))}
+            style={{ width: "100%", justifyContent: "center" }}
+          >
+            <img
+              src={emojimap[interacciones.subcategoria] || "/default.svg"}
+              alt="Emoji Icon"
+              className="w-6 md:w-8 h-6 md:h-8 ml-2"
+            />
+            {interacciones.subcategoria}
+          </span>
+        </div>
+      </td>
+      <td className="py-4 md:py-6 px-4 md:px-6 text-left hidden sm:table-cell">{interacciones.fecha}</td>
+    </tr>
+  ))}
+</tbody>
         </table>
       </div>
     </div>
