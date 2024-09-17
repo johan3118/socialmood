ed9a32cae5954a689c82cee9bedec9a72de24db8
Paola <saldanaquezada@gmail.com>
2024-09-17 11:47:30 -0400

    page de listado de interacciones creado

---
+++ src/app/(socialmood)/app/listado-interacciones/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListadoInteraccionesTable from '@/components/(socialmood)/listado-interacciones-table'
+
+function ListadoInteraccionesPage() {
+  return (
+    <div>
+        <ListadoInteraccionesTable/>
+    </div>
+  )
+}
+
+export default ListadoInteraccionesPage
