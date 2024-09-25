17b74c46084338a444bcbbc35f7eab78bc343d06
pazzispb <pazzispb20@gmail.com>
2024-09-25 00:04:23 -0400

    close button deleted from dialog content

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -45,10 +45,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
         {children}
-        <DialogPrimitive.Close className="absolute right-6 top-6 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground text-white">
-          <img src="/delete.svg" alt="Close" className="w-6 h-6" />
-          <span className="sr-only">Close</span>
-        </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
