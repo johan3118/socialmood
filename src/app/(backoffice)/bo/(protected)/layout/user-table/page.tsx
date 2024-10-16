2d27b3a81c32acdf500cfd10f8fd77ad23ca3483
pazzispb <pazzispb20@gmail.com>
2024-10-15 22:46:50 -0400

    add user button added

--- src/app/(backoffice)/bo/(protected)/layout/user-table/page.tsx
+++ src/app/(backoffice)/bo/(protected)/layout/user-table/page.tsx
@@ -1,11 +1,23 @@
+"use client"
 import React from 'react'
+import {useRouter} from 'next/navigation';
 
-function ListadoSubscripcionesPage() {
+
+function ListadoUsuariosPage() {
+  const router = useRouter();
+
+  const handleCrearSubscripcion = () => {
+    router.push("/bo/create-user");
+  };
+
   return (
     <div>
         <h1> Listado de usuarios </h1>
+        <button className="btn w-8 h-8 bg-[#D24EA6] rounded-lg" onClick={handleCrearSubscripcion}>
+          <span className="text-white text-2xl">+</span>
+        </button>
     </div>
   )
 }
 
-export default ListadoSubscripcionesPage
+export default ListadoUsuariosPage
