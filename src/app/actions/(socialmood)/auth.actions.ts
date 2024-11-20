dff7dba8c84ce085ef3ba6463dc9f2b395b9b01d
Paola <saldanaquezada@gmail.com>
2024-11-20 09:51:57 -0400

    funcionalidad actualizar datos de perfil

--- src/app/actions/(socialmood)/auth.actions.ts
+++ src/app/actions/(socialmood)/auth.actions.ts
@@ -280,3 +280,92 @@ export async function getActiveUserEmail() {
 
   return result[0].correo_electronico.toString();
 }
+
+
+export async function getActiveUserAddress() {
+  // Validar la sesión activa y obtener el usuario autenticado
+  const { user } = await validateRequest();
+
+  // Obtener el ID del usuario
+  const userId = user?.id;
+
+  // Verificar si el ID del usuario está definido
+  if (userId === undefined) {
+    return {
+      error: "User ID is undefined",
+    };
+  }
+
+  try {
+    const result = await db
+      .select({ direccion: usuariosTable.direccion })
+      .from(usuariosTable)
+      .where(eq(usuariosTable.id, userId))
+      .limit(1);
+
+    // Verificar si se obtuvo un resultado
+    if (result.length === 0) {
+      return {
+        error: "Address not found for the user",
+      };
+    }
+
+    // Retornar la dirección del usuario
+    return result[0].direccion.toString();
+  } catch (error: any) {
+    return {
+      error: error?.message || "An error occurred while fetching the address",
+    };
+  }
+}
+
+export async function updateUserProfile(data: {
+  name: string;
+  lastName: string;
+  address: string;
+}) {
+  // Validar la sesión activa y obtener el usuario autenticado
+  const { user } = await validateRequest();
+
+  // Obtener el ID del usuario // en el backoffice filtrar por email
+  const userId = user?.id;
+
+  // Verificar si el ID del usuario está definido
+  if (userId === undefined) {
+    return {
+      error: "User ID is undefined",
+    };
+  }
+
+  // Obtener los datos del usuario
+  const { name, lastName, address } = data;
+
+  // Verificar si los datos del usuario están definidos
+  if (!name || !lastName  || !address) {
+    return {
+      error: "Missing user data",
+    };
+  }
+
+  try {
+    // Actualizar los datos del usuario
+    await db
+      .update(usuariosTable)
+      .set({
+        nombre: name,
+        apellido: lastName,
+        direccion: address,
+      })
+      .where(eq(usuariosTable.id, userId));
+
+    // Retornar una respuesta exitosa
+    return {
+      success: true,
+    };
+  } catch (error: any) {
+    return {
+      error: error?.message || "An error occurred while updating the user profile",
+    };
+  }
+}
+
--- src/components/(socialmood)/social-media-card.tsx
+++ src/components/(socialmood)/social-media-card.tsx
@@ -51,7 +51,7 @@ const SocialMediaCard: React.FC = () => {
           <AddSocialForm onClose={toggleModal} onFormSubmit={fetchPerfiles} />
         </Modal>
       )}
-      <BlurredContainer customStyle="h-[30vh] !m-0">
+      <BlurredContainer customStyle="h-[30vh]">
         <div className="flex items-center justify-between w-full mb-3">
           <h2 className="text-2xl font-bold">Redes Sociales</h2>
           <div className="options flex items-center">
