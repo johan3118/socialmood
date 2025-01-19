6ee5245417fb86ac0b2d0cc49fc837454d2f7cd4
Johan Contreras <cepedaabiel@gmail.com>
2025-01-19 18:18:41 -0400

    merge develop google idioma

---
+++ .history/src/components/(backoffice)/profile-settings_20250119181518.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ArrowLeft, Edit, Plus, X } from "lucide-react";
+import { User } from "lucide-react";
+import UserSettingsCard from "./user-settings-card";
+import TranslatePage from "../(socialmood)/translate-page";
+
+interface ProfileSettingsProps {
+  onClose: () => void;
+}
+
+export default function ProfileSettings({ onClose }: ProfileSettingsProps) {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 px-[25vh]">
+      <div className="bg-[#2C2436] text-white p-10 rounded-3xl w-full overflow-y-auto">
+        <header className="flex items-center mb-6">
+          <button onClick={onClose} className="mr-4" aria-label="Volver">
+            <ArrowLeft className="w-6 h-6" />
+          </button>
+          <h1 className="text-3xl font-bold">Ajustes de Perfil</h1>
+        </header>
+        <div className="flex justify-center items-center">
+          {/* User Card */}
+          <UserSettingsCard />
+          {/* User Card */}
+          <TranslatePage />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ .history/src/components/(backoffice)/user-settings-card_20250119181637.tsx
@@ -0,0 +1,189 @@
+"use client";
+
+import { Edit, Save, X } from "lucide-react"; // Importar el icono X
+import BlurredContainer from "@/components/(socialmood)/blur-background";
+import React, { useEffect, useState } from "react";
+import {
+  getActiveUserName,
+  getActiveUserEmail,
+  getActiveUserAddress,
+  updateUserProfile,
+} from "@/app/actions/(backoffice)/auth.actions";
+import { toast } from "@/components/ui/use-toast";
+
+function UserSettingsCard() {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userAddress, setUserAddress] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  // Fetch current user data
+  const fetchUserData = async () => {
+    const nameResult = await getActiveUserName();
+    if (typeof nameResult === "string") {
+      const [name, surname] = nameResult.split(" ");
+      setFirstName(name || "");
+      setLastName(surname || "");
+    } else {
+      console.error(nameResult.error);
+    }
+
+    const emailResult = await getActiveUserEmail();
+    if (typeof emailResult === "string") {
+      setUserEmail(emailResult);
+    } else {
+      console.error(emailResult.error);
+    }
+
+    const addressResult = await getActiveUserAddress();
+    if (typeof addressResult === "string") {
+      setUserAddress(addressResult);
+    } else {
+      console.error(addressResult.error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  // Update user data
+  const handleSaveChanges = async () => {
+    if (!firstName || !lastName || !userEmail || !userAddress) {
+      toast({
+        variant: "destructive",
+        description: "Por favor, llena todos los campos obligatorios.",
+      });
+      return;
+    }
+
+    try {
+      const response = await updateUserProfile({
+        name: firstName,
+        lastName: lastName,
+        address: userAddress,
+      });
+      if (response.success) {
+        toast({
+          variant: "default",
+          description: "Perfil actualizado exitosamente.",
+        });
+        setIsEditing(false);
+      } else {
+        console.error(response.error);
+      }
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Error al guardar los cambios",
+      });
+      console.error("Error al guardar los cambios:", error);
+    }
+  };
+
+  // Cancel editing
+  const handleCancel = () => {
+    fetchUserData(); // Restablece los datos del usuario
+    setIsEditing(false);
+  };
+
+  return (
+    <BlurredContainer customStyle="h-auto !m-0 p-6">
+      <div className="flex flex-col gap-y-4 w-full">
+        <div className="flex items-center gap-x-3">
+          <div className="w-20 h-20 text-5xl bg-emerald-500 rounded-full flex items-center justify-center">
+            😎
+          </div>
+          <div className="flex-1">
+            <h2 className="font-bold text-[26px]">Datos de Perfil</h2>
+          </div>
+
+          {/* Botones de acción */}
+          {!isEditing ? (
+            <button
+              className="text-white flex items-center gap-1"
+              onClick={() => setIsEditing(true)}
+            >
+              <Edit className="w-6 h-6" />
+            </button>
+          ) : (
+            <div className="flex items-center gap-2">
+              <button
+                className="text-green-500 hover:underline flex items-center gap-1"
+                onClick={handleSaveChanges}
+              >
+                <Save className="w-6 h-6" />
+              </button>
+              <button
+                className="text-white hover:underline flex items-center gap-1"
+                onClick={handleCancel}
+              >
+                <X className="w-6 h-6" />
+              </button>
+            </div>
+          )}
+        </div>
+
+        {/* Editable fields */}
+        <div className="flex flex-col gap-y-3">
+          <div className="flex gap-x-3">
+            <div className="flex-1">
+              <label className="text-sm font-medium">Nombre</label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                className={`bg-white text-black rounded-lg py-2 w-full ${
+                  isEditing
+                    ? "border-none"
+                    : "bg-white/30 border border-gray-300 text-white"
+                }`}
+                disabled={!isEditing}
+              />
+            </div>
+            <div className="flex-1">
+              <label className="text-sm font-medium">Apellido</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                className={`bg-white text-black rounded-lg py-2 w-full ${
+                  isEditing
+                    ? "border-none"
+                    : "bg-white/30 border border-gray-300 text-white"
+                }`}
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div>
+            <label className="text-sm font-medium">Dirección</label>
+            <input
+              type="text"
+              value={userAddress}
+              onChange={(e) => setUserAddress(e.target.value)}
+              className={`bg-white text-black rounded-lg py-2 w-full ${
+                isEditing
+                  ? "border-none"
+                  : "bg-white/30 border border-gray-300 text-white"
+              }`}
+              disabled={!isEditing}
+            />
+          </div>
+          <div>
+            <label className="text-sm font-medium">Correo electrónico</label>
+            <input
+              type="email"
+              value={userEmail}
+              disabled={true}
+              className={`bg-white rounded-lg py-2 w-full bg-white/30 border border-gray-300 text-white`}
+            />
+          </div>
+        </div>
+      </div>
+    </BlurredContainer>
+  );
+}
+
+export default UserSettingsCard;
